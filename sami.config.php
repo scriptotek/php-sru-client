f06b0f5abfd9d3e187f9165401be4d0679f73443
Dan Michael O. Heggø <danmichaelo@gmail.com>
2021-12-19 21:15:33 +0100

    Remove sami config

--- sami.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return new Sami\Sami(__DIR__ . '/src', array(
-    'title' => 'SRU client',
-    'build_dir' => __DIR__ . '/api_docs',
-    'cache_dir' => __DIR__ . '/cache',
-    'default_opened_level' => 2,
-));
-
